39874803e1e0f7c965a58739bfd443973f5e7fa8
Pavel Alekseev <pavelbooking.com@yandex.ru>
2022-12-23 15:46:36 +0500

    new images

--- src/Contacts/Contacts.jsx
+++ src/Contacts/Contacts.jsx
@@ -5,6 +5,10 @@ const Contacts = (props) => {
 
   return (
     <div className='contacts'>
+      <div className="contacts__headers">
+            <h3 className="contacts__header3">Контакты</h3>
+            <h2 className="contacts__header2">Свяжитесь со мной</h2>
+        </div>
       <button onClick={props.onSendForm}>Форма обратной связи</button>
     </div>
   );
