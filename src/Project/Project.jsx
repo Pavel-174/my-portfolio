0778ffdca387d43967ae512d36a514af8c642ce8
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-01-03 12:43:07 +0500

    fixed some bugs

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -11,8 +11,8 @@ function Project({onCardClick, project, id, src, title, site, git}) {
         <img className="portfolio__img" src={src} alt={title}></img>
         <div className="portfolio__links">
           <h5>{title}</h5>
-          <a className="portfolio__link" href={site}><img src="https://raw.githubusercontent.com/Pavel-174/my-portfolio/ebfda2bae11af2cfdc4f3d93360cf65cc0121304/src/images/globe.svg"></img></a>
-          <a className="portfolio__link" href={git}><img src="https://github.com/Pavel-174/my-portfolio/blob/master/src/images/github.png?raw=true"></img></a>
+          <a className="portfolio__link" href={site} onClick={(e) => {e.stopPropagation();}}><img src="https://raw.githubusercontent.com/Pavel-174/my-portfolio/ebfda2bae11af2cfdc4f3d93360cf65cc0121304/src/images/globe.svg"></img></a>
+          <a className="portfolio__link" href={git} onClick={(e) => {e.stopPropagation();}}><img src="https://github.com/Pavel-174/my-portfolio/blob/master/src/images/github.png?raw=true"></img></a>
         </div>
       </li>
     )
